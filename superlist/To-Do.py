32f89224638decdd9a8dfe396a37fc80936344b7
SilverYoung <yytbt1@foxmail.com>
2017-10-04 19:09:14 +0800

    render home_page successful

--- superlist/To-Do.py
+++ superlist/To-Do.py
@@ -1,4 +1,27 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        # self.browser.quit()
+        pass
+
+    # 必须以test_开头
+    # def test_get_url_with_to_do_list(self):
+    #     self.browser.get('http://localhost:8000')
+    #     self.assertIn('To-Do', self.browser.title)
+    #     self.fail('Finish the test!')
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -8,3 +9,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         fond = resolve('/')
         self.assertEqual(fond.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
+        self.assertIn(b'<title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
