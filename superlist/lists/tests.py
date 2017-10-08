1a63350a470b710442e3852bffcff033fd178621
SilverYoung <yytbt1@foxmail.com>
2017-10-08 17:01:35 +0800

    添加静态外键

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your tests here.
@@ -20,18 +20,33 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.endswith(b'</html>'))
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The First!'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The Second!'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, second_item.text)
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class NewListTest(TestCase):
@@ -56,8 +71,11 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_lists(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List()
+        list_.save()
+
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the_only_list/')
 
--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -19,6 +19,9 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List()
+    list_.save()
+
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the_only_list/')
 
