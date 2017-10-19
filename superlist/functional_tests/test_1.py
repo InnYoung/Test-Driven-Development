b6394d99d1d9432574d0554d094b8a857e4c5075
SilverYoung <yytbt1@foxmail.com>
2017-10-19 18:00:27 +0800

    read css

--- superlist/functional_tests/test_1.py
+++ superlist/functional_tests/test_1.py
@@ -5,10 +5,11 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
